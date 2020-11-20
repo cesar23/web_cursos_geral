42a3133fee4fcf48d93218d0628124e4bb5fc4b5
Cesar Auris Casa <perucaos@gmail.com>
2020-11-20 18:57:33 -0500

    cmder se actualizo :

---
+++ gitup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+function upgit() {
+	git pull
+    git add -A
+    git commit -m "cmder se actualizo :${DATE_HOUR_GIT}"
+    git push -u origin master
+}
+
+function gitup() {
+    git pull
+	  git add -A
+    git commit -m "cmder se actualizo :${DATE_HOUR_GIT}"
+    git push -u origin master
+}
+
+function gitup2() {
+    git pull
+	  git add -A
+    git commit -m "cmder se actualizo :${DATE_HOUR_GIT}"
+    git push
+}
+
+gitup
+
+echo ""
+echo "  ██████  ██   ██                ██████ ███████ ███████  █████  ██████  "
+echo " ██    ██ ██  ██                ██      ██      ██      ██   ██ ██   ██ "
+echo " ██    ██ █████       █████     ██      █████   ███████ ███████ ██████  "
+echo " ██    ██ ██  ██                ██      ██           ██ ██   ██ ██   ██ "
+echo "  ██████  ██   ██                ██████ ███████ ███████ ██   ██ ██   ██ "
+echo ""
+echo ""
+sleep 4
